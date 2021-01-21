9b7c67cb8ae96774929b92b90a2a71a1e8f2935b
Anders Lundback <lundback.anders@gmail.com>
2021-01-21 19:13:39 +0000

    feat: space out components using our custom Spacer component

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = ({ navigation }) => {
     return (
         <>
-            <Text h3>Sign Up for Tracker</Text>
-            <Input label="Email" />
-            <Input label="Password" />
-            <Button title="Sign Up" />
+            <Spacer>
+                <Text h3>Sign Up for Tracker</Text>
+            </Spacer>
+            <Spacer>
+                <Input label="Email" />
+            </Spacer>
+            <Spacer>
+                <Input label="Password" />
+            </Spacer>
+            <Spacer>
+                <Button title="Sign Up" />
+            </Spacer>
         </>
     )
 };
