baa6045abffa6a6ce9a8704ca4604200b0f1bb11
Anders Lundback <lundback.anders@gmail.com>
2021-01-21 21:03:38 +0000

    feat: /signup to track-server with basic error handling

---
+++ src/api/tracker.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://3f9d5c7ef0a6.ngrok.io'
+});
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,16 +1,27 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
+
+const ADD_ERROR = 'ADD_ERROR';
 
 const authReducer = (state, action) => {
     switch (action.type) {
+        case ADD_ERROR:
+            return { ...state, errorMessage: action.payload };
         default:
             return state;
     };
 };
 
-const signup = (dispatch) => {
-    return ({ email, password }) => {
+const signup = dispatch => {
+    return async ({ email, password }) => {
         // make API request to sign up with that email and password
+        try {
+            const response = await trackerApi.post('/signup', { email, password });
+            console.log(response.data);
+        } catch (err) {
+            dispatch({ type: ADD_ERROR, payload: 'Something went wrong with sign up' })
 
+        }
         // if we sign up, modify out state, and say the we are authenticated
 
         // if signing up fails, we'd need to reflect an error message somewhere
@@ -37,5 +48,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
     authReducer,
     { signin, signout, signup },
-    { isSignedIn: false }
+    { isSignedIn: false, errorMessage: '' }
 );
