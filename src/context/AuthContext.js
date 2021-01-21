3bb5da709681bd8ba868d5331235ff4ceb3342ad
Anders Lundback <lundback.anders@gmail.com>
2021-01-21 19:47:41 +0000

    feat: scaffold auth functions

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,11 +4,38 @@ const authReducer = (state, action) => {
     switch (action.type) {
         default:
             return state;
-    }
+    };
 };
+
+const signup = (dispatch) => {
+    return ({ email, password }) => {
+        // make API request to sign up with that email and password
+
+        // if we sign up, modify out state, and say the we are authenticated
+
+        // if signing up fails, we'd need to reflect an error message somewhere
+
+    };
+};
+
+const signin = (dispatch) => {
+    return ({ email, password }) => {
+        // try to sign in
+        // handle success by updating state
+        // handle failure by displaying error msg
+    };
+};
+
+const signout = (dispatch) => {
+    return () => {
+        // somehow sign out!
+    };
+};
+
+
 
 export const { Provider, Context } = createDataContext(
     authReducer,
-    {},
+    { signin, signout, signup },
     { isSignedIn: false }
 );
